45ef0a63860d14b1abd57d2c41f92bef307a552b
jseo9732 <jseo9732@gmail.com>
2021-10-04 23:11:00 +0900

    Refactor: Securing the Keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase/compat/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDjlUMhU-UkbZpULCfvkulew2TxDeF2VBA",
-  authDomain: "nwitter-ba414.firebaseapp.com",
-  projectId: "nwitter-ba414",
-  storageBucket: "nwitter-ba414.appspot.com",
-  messagingSenderId: "1042124446484",
-  appId: "1:1042124446484:web:619abaf7a353a5e8d74001"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
