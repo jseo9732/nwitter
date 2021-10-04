04154fe04b86585f0c44c2a5f017ecc357b9b1ac
jseo9732 <jseo9732@gmail.com>
2021-10-04 23:02:22 +0900

    Feat: Setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/compat/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjlUMhU-UkbZpULCfvkulew2TxDeF2VBA",
+  authDomain: "nwitter-ba414.firebaseapp.com",
+  projectId: "nwitter-ba414",
+  storageBucket: "nwitter-ba414.appspot.com",
+  messagingSenderId: "1042124446484",
+  appId: "1:1042124446484:web:619abaf7a353a5e8d74001"
+};
+
+export default firebase.initializeApp(firebaseConfig);
