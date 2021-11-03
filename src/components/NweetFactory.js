0e10a6044b1cb02e0e8a17d3a4f49daf26b641b3
jseo9732 <jseo9732@gmail.com>
2021-11-04 01:43:17 +0900

    Update NweetFactory.js

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -55,7 +55,9 @@ const NweetFactory = ({ userObj }) => {
       } = finishedEvent;
       setAttachment(result);
     };
-    reader.readAsDataURL(theFile);
+    if (Boolean(theFile)) {
+      reader.readAsDataURL(theFile);
+    }
   };
   const onClearPhotoClick = () => {
     setAttachment("");
